ed8d7f2a707054cbcad534a0e8c1a0781d425673
StarBurst <panda.code.lab@gmail.com>
2021-03-18 02:13:58 +0300

    done_3

---
+++ pages/chat.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>CHAT PAGE</h1>
+    <h2>{{ user.name }}</h2>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  middleware: ["chat"],
+  head() {
+    return {
+      title: `Комната ${this.user.room}`
+    };
+  },
+  computed: {
+    ...mapState(["user"])
+  }
+};
+</script>
